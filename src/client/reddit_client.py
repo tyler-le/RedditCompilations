210abddce059c8a5ffa844093154e8bd115c5caf
Tyler Le <tylerle99@yahoo.com>
2025-02-24 22:19:26 -0800

    change from top of the week to hot

--- src/client/reddit_client.py
+++ src/client/reddit_client.py
@@ -51,7 +51,7 @@ class RedditWrapper:
 
         subreddit = self.reddit.subreddit(subreddit_name)
 
-        for post in subreddit.top(time_filter="week"):
+        for post in subreddit.hot():
             if not post.is_video:
                 continue
             print(f"🎬 Found video: {post.title}")
