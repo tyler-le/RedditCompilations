3e86f556600aa14968635320ef56c0e31d1cadf4
Tyler Le <tylerle99@yahoo.com>
2025-03-01 17:09:50 -0800

    change to top of the week

--- src/client/reddit_client.py
+++ src/client/reddit_client.py
@@ -56,7 +56,7 @@ class RedditWrapper:
 
         subreddit = self.reddit.subreddit(subreddit_name)
 
-        for post in subreddit.hot():
+        for post in subreddit.top("week"):
             if not post.is_video:
                 continue
             print(f"🎬 Found video: {post.title}")
